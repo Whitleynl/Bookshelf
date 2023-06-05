6e1c03ddd4fa0989c5f39e21af6f3cb34311d8d3
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-06-05 08:56:49 -0400

    renamed some files

--- APP3/src/main/java/org/example/app3/BookshelfApp.java
+++ APP3/src/main/java/org/example/app3/BookshelfApp.java
@@ -1,16 +1,15 @@
 package org.example.app3;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class BookshelfApp extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BookshelfApp.class.getResource("bookshelf.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Bookshelf");
         stage.setScene(scene);
@@ -18,6 +17,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        Application.launch(args);
+        javafx.application.Application.launch(args);
     }
 }
--- APP3/src/main/java/org/example/app3/BookshelfController.java
+++ APP3/src/main/java/org/example/app3/BookshelfController.java
@@ -3,12 +3,12 @@ package org.example.app3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class BookshelfController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Bookshelf!");
     }
 }
