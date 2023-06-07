5ada99a5230ed426922299d5ddb4ecbacfca8638
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-06-06 23:03:31 -0400

    app displays sample data

--- APP3/src/main/java/org/example/app3/BookshelfController.java
+++ APP3/src/main/java/org/example/app3/BookshelfController.java
@@ -1,14 +1,19 @@
 package org.example.app3;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class BookshelfController {
+    public Label welcomeText;
     @FXML
-    private Label welcomeText;
+    private ListView<String> bookListView;
+    @FXML
+    protected void handleStartButtonAction() {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Bookshelf!");
+        ObservableList<String>bookList = FXCollections.observableArrayList("Book 1", "Book 2", "Book 3");
+        bookListView.setItems(bookList);
     }
 }
